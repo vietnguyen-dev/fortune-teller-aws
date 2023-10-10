c561064fece208241ab3bc13efd43262c902305f
vietnguyen-dev <77704810+vietnguyen-dev@users.noreply.github.com>
2023-10-10 16:38:58 -0700

    added clear button

--- fortune-teller-lambda/index.js
+++ fortune-teller-lambda/index.js
@@ -1,29 +1,11 @@
 export const handler = async (event) => {
-  try {
-    const options = ["yes", "no"];
-    const randomIndex = Math.floor(Math.random() * options.length);
-    // TODO implement
-    const response = {
-      statusCode: 200,
-      headers: {
-        "Access-Control-Allow-Origin": "*", // Required for CORS support to work
-        "Access-Control-Allow-Credentials": true, // Required for cookies, authorization headers with HTTPS
-      },
-      body: JSON.stringify(options[randomIndex]),
-    };
-    return response;
-  }
-  catch(err) {
-    const response = {
-      statusCode: 500,
-      // headers: {
-      //   "Access-Control-Allow-Origin": "*", // Required for CORS support to work
-      //   "Access-Control-Allow-Credentials": true, // Required for cookies, authorization headers with HTTPS
-      // },
-      body: JSON.stringify('Error'),
-    }
-    return response;
-
+  const options = ["yes", "no"];
+  const randomIndex = Math.floor(Math.random() * options.length);
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify(options[randomIndex]),
   };
+  return response;
 };
+
   
